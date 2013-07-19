b5ca7fad892ea56cbe89f2027efee2be98a22e01
FENG Zhichao <flankerfc@gmail.com>
2013-07-19 18:44:29 +1000

    it should be able to add new item.

--- spec/views/todo-spec.js
+++ spec/views/todo-spec.js
@@ -1,5 +1,13 @@
+$.fn.pressEnter = function () {
+    var e = $.Event("keypress");
+    e.keyCode = 13;
+    $(this).trigger(e);
+};
+
 jasmine.getFixtures().fixturesPath = 'spec/fixtures';
+
 describe("todo view", function(){
+
     beforeEach(function(){
         loadFixtures('templ.html');
     });
@@ -19,13 +27,15 @@ describe("todo view", function(){
         expect($("#todos").find(".todo").length).toBe(3);
     });
 
-    // it("should be able to add new item to list", function() {
-    //     $("input").todoify({
-    //         container: "#todos"
-    //     });
-    //     expect($("#todos").find(".todo").length).toBe(0);
+    it("should be able to add new item to list", function() {
+        $("input").todoify({
+            container: "#todos"
+        });
+        expect($("#todos").find(".todo").length).toBe(0);
 
-    //     $("input").val("new item added.").;
-    //     expect($("#todos").find(".todo").length).toBe(1);
-    // });
-})
+        $("input").val("new item added.").pressEnter();
+        expect($("#todos").find(".todo").length).toBe(1);
+        expect($("#todos").find(".todo").first()).toContainText("new item added.");
+    });
+
+});
